965c3b3ce32ff3443708a07322e8fcde0e9104b6
Dan Alvarez <danalvarez5280@gmail.com>
2017-07-10 16:49:58 -0600

    Added working bubble sort and a working insertion sort

---
+++ scripts/bubbleSort.js
@@ -0,0 +1,28 @@
+let numNums = [];
+let count = 0;
+
+for (let n = 0; n < 100; n++ ){
+  let nums = Mth.floor(Math.random() * (100 - 1) + 1);
+  numNums.push(nums)
+
+}
+
+const bubbleSort = array => {
+  if (Array.isArray(array) && array.length > 0){
+    for (let i = array.length - 1; i > 0; i--) {
+
+      for (let j = 0; j < i; j++) {
+        let n = j + 1;
+
+        while (array[j] > array[n]) {
+          [array[j], array[n]] = [array[n], array[j]]
+        }
+        count++
+        console.log(count)
+      }
+    }
+    return array
+  }
+}
+
+bubbleSort(numNums)
